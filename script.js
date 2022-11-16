f5655e584de1ab1d1ac99e18b9abd11d8f423f2e
Ichigo Kurosaki <nfragale01@gmail.com>
2022-11-15 19:17:59 -0500

    Add Function Name to Operate.

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ function divide(x,y) {
     return x / y;
 }
 
-//#2 create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-function(operator, x, y){
+// #2 create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(operator, x, y) {
     if(operator == 'divide') {
         return divide(x,y);
     }
