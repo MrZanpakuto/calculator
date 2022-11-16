e20d5b8136d6a0f5843bbb0d57d4749ed3f7e7d3
Ichigo Kurosaki <nfragale01@gmail.com>
2022-11-15 19:04:45 -0500

    Add Basic Math Functions and Operate Function.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//#1 basic math operators
+
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+//#2 create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function(operator, x, y){
+    if(operator == 'divide') {
+        return divide(x,y);
+    }
+}
